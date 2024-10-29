336960976353e779b1eaf9d983c7fec0e93e0e15
antonisoaho <isoahoanton@gmail.com>
2024-10-29 15:23:02 +0100

    setup corsOptions

--- config.js
+++ config.js
@@ -9,22 +9,23 @@ console.log("PROCCESS", process.env.DEV_PORT);
 const mongoose = require("mongoose");
 
 const clientOptions = {
-    serverApi: {
-      version: "1",
-      strict: true,
-      deprecationErrors: true,
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      strictPopulate: false,
-    },
-  };
+  serverApi: {
+    version: "1",
+    strict: true,
+    deprecationErrors: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    strictPopulate: false,
+  },
+};
 
-  module.exports = {
-    dev_uri: process.env.DEV_URI,
-    dev_url: process.env.DEV_URL,
-    dev_port: process.env.DEV_PORT,
-    dev_secret: process.env.DEV_SECRET,
-    clientOptions,
-    mongoose,
-    express,
-  }
+module.exports = {
+  dev_uri: process.env.DEV_URI,
+  dev_url: process.env.DEV_URL,
+  dev_port: process.env.DEV_PORT,
+  dev_secret: process.env.DEV_SECRET,
+  origin_url: process.env.ORIGIN_URL,
+  clientOptions,
+  mongoose,
+  express,
+};
