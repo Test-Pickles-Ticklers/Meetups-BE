70b4203623e57f491ea6176e9d222ece0f291b03
antonisoaho <isoahoanton@gmail.com>
2024-10-31 13:54:34 +0100

    Fixed some small errors

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -6,50 +6,63 @@ const {
   getParticipation,
   loginUser,
   registerUser,
+  getOwnMeetups,
 } = require("../Services/UsersService");
 
-user.post("/signup", async (req, res) => {
-  try {
-    const { email, password } = req.body;
-    const user = await registerUser(email, password);
+user
+  .post("/signup", async (req, res) => {
+    try {
+      const { email, password } = req.body;
+      const user = await registerUser(email, password);
 
-    res.status(201).send(user);
-  } catch (error) {
-    res.status(400).send(error);
-  }
-});
+      res.status(201).send(user);
+    } catch (error) {
+      res.status(400).send(error);
+    }
+  })
 
-user.post("/login", async (req, res) => {
-  try {
-    const { email, password } = req.body;
-    const token = await loginUser(email, password);
+  .post("/login", async (req, res) => {
+    try {
+      const { email, password } = req.body;
+      const token = await loginUser(email, password);
 
-    res.status(200).send({ token, email });
-  } catch (error) {
-    res.status(error.status).send({ error: error.message });
-  }
-});
+      res.status(200).send({ token, email });
+    } catch (error) {
+      res.status(error.status).send({ error: error.message });
+    }
+  })
 
-user.get("/meetups", auth, async (req, res) => {
-  try {
-    const { email } = req.user;
-    const data = await getParticipation(email);
+  .get("/meetups", auth, async (req, res) => {
+    try {
+      const { email } = req.user;
+      const data = await getParticipation(email);
 
-    res.status(200).send(data);
-  } catch (error) {
-    res.status(500).send({ error: error.message });
-  }
-});
+      res.status(200).send(data);
+    } catch (error) {
+      res.status(500).send({ error: error.message });
+    }
+  })
 
-user.get("/reviews", auth, async (req, res) => {
-  try {
-    const { email } = req.user;
-    const data = await getAllReviewsByUser(email);
+  .get("/reviews", auth, async (req, res) => {
+    try {
+      const { email } = req.user;
+      const data = await getAllReviewsByUser(email);
 
-    res.status(200).send(data);
-  } catch (error) {
-    res.status(500).send({ error: error.message });
-  }
-});
+      res.status(200).send(data);
+    } catch (error) {
+      res.status(500).send({ error: error.message });
+    }
+  })
+
+  .get("/organizedMeetups", auth, async (req, res) => {
+    try {
+      const { email } = req.user;
+      const data = await getOwnMeetups(email);
+
+      res.status(200).send(data);
+    } catch (error) {
+      res.status(500).send({ error: error.message });
+    }
+  });
 
 module.exports = { user };
